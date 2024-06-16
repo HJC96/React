05c5873ea393b26d8ede81cc34b5b2f5146df208
HJC96 <gkswlcjs2@naver.com>
2024-06-16 23:30:52 +0900

    1. Tic-Tac-Toe 게임 - 콜백함수 구현 진행중

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './styles.css'
+import { useState } from 'react';
 
-function App() {
+interface SquareProps {
+  value: string;
+  onSquareClick: () => void;
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(''));
+
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => {}} />
+        <Square value={squares[1]} onSquareClick={() => {}} />
+        <Square value={squares[2]} onSquareClick={() => {}} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => {}} />
+        <Square value={squares[4]} onSquareClick={() => {}} />
+        <Square value={squares[5]} onSquareClick={() => {}} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => {}} />
+        <Square value={squares[7]} onSquareClick={() => {}} />
+        <Square value={squares[8]} onSquareClick={() => {}} />
+      </div>
+    </>
   );
 }
 
-export default App;
+function Square({ value, onSquareClick }: SquareProps) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
+}
